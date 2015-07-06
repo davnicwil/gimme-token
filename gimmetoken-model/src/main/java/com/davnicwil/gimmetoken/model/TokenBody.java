33feed1d57834ba0027b86a36fb4c210181ec1a7
Dave Williams <davnicwil@gmail.com>
2015-07-06 16:07:16 -0500

    add default constructor for Jackson to use

--- gimmetoken-model/src/main/java/com/davnicwil/gimmetoken/model/TokenBody.java
+++ gimmetoken-model/src/main/java/com/davnicwil/gimmetoken/model/TokenBody.java
@@ -4,6 +4,8 @@ public class TokenBody {
 
     public String authToken;
 
+    public TokenBody() {} // for Jackson
+
     public TokenBody(String authToken) {
         this.authToken = authToken;
     }
